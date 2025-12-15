5c7be37ec62d8d24b618dd425401312c68351ebb
sahitya12 <66714414+sahitya12@users.noreply.github.com>
2025-12-15 14:22:12 +0530

    Update variables.tf

--- tags/variables.tf
+++ tags/variables.tf
@@ -1,6 +1,7 @@
 variable "tags_to_be_added" {
   description = "Map of Azure resourceId => map(tags)"
   type        = map(map(string))
+  default     = {}
 }
 
 variable "sql_warehouse_desired_tags" {
@@ -28,6 +29,6 @@ variable "adh_standard_tags_update_delay_in_seconds" {
 
 variable "databricks_compute_tags_use_json_file_as_source_of_truth" {
   type        = string
-  description = "If true, only tags in inputs remain on compute; others are removed."
+  description = "If true, only tags in inputs remain on compute; other tags removed."
   default     = "false"
 }
