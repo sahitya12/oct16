2c64f542f8beb8d9c75ea10431715d89910e64f1
sahitya12 <66714414+sahitya12@users.noreply.github.com>
2025-12-15 14:21:16 +0530

    Create output.tf

---
+++ tags/standard_tags/output.tf
@@ -0,0 +1,11 @@
+output "tags_to_be_added" {
+  value = local.tags_to_be_added
+}
+
+output "sql_warehouse_desired_tags" {
+  value = local.sql_warehouse_desired_tags
+}
+
+output "databricks_compute_tags" {
+  value = local.databricks_compute_tags
+}
